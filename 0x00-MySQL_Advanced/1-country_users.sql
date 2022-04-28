700acedddbd4259bd15d2f6eb0cd083b7f9a16e9
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-28 12:00:36 -0500

    country fixed

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -10,6 +10,6 @@ CREATE TABLE `users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL UNIQUE,
   `name` varchar(255),
-  `country` varchar(2),
+  `country` ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
   PRIMARY KEY (`id`)
 );
