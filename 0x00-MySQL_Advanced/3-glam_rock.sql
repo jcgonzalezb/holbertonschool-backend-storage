0a8d5d2ac07791d5fa915bbde1208a5a290d8ada
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-28 12:06:38 -0500

    IFNULL replace for 2020 used

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,7 +3,7 @@
 -- Column names must be: band_name and lifespan (in years)
 -- You should use attributes formed and split for computing the lifespan
 
-SELECT band_name, split - formed as lifespan
+SELECT band_name, IFNULL(split, 2020) - formed as lifespan
 FROM metal_bands
 WHERE style LIKE ('%Glam rock%')
 ORDER BY lifespan DESC;
