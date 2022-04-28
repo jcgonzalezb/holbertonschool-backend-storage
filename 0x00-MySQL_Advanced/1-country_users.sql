dfdc5384677a10a119502815cd98a023b7dd5d86
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-28 12:02:20 -0500

    syntax fixed

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -10,6 +10,6 @@ CREATE TABLE `users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL UNIQUE,
   `name` varchar(255),
-  `country` ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+  `country` ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
   PRIMARY KEY (`id`)
 );
