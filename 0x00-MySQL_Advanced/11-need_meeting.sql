74df6f12a5f7f51c8489141bb1cb9fe94462c6f2
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-30 12:59:04 -0500

    date fixed

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,6 +2,6 @@
 -- The view need_meeting should return all students name when They score are under (strict) to 80 AND no last_meeting date OR more than a month
 
 delimiter |
-CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND last_meeting is NULL OR MONTH(last_meeting) > 1;
+CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND last_meeting is NULL OR last_meeting < date_sub(now(), interval 1 month);
 |
 delimiter ;
