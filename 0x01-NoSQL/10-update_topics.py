abc359caffa94ed9c3b10b9605b03d419e0b783c
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-02 20:54:34 -0500

    my_query updated

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This project module contains a function that changes
+all topics of a school document based on the name.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Function that changes all topics of a school
+    document based on the name.
+    name (string) will be the school name to update.
+    topics (list of strings) will be the list of
+    topics approached in the school
+    Returns:
+        updated topics
+    """
+    myquery = { "name": name }
+    newvalues = { "$set": { "topics": topics } }
+
+    mongo_collection.update_one(myquery, newvalues)
