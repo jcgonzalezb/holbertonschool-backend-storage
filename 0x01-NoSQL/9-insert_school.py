00976f9c0dc23f73fbd4233552b00e477054d340
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-02 19:46:56 -0500

    return fixed

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This project module contains a function that inserts
+a new document in a collection based on kwargs
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Function that inserts a new document in a
+    collection based on kwargs.
+    Returns:
+        The new _id
+    """
+    result = mongo_collection.insert_one(kwargs)
+
+    return result.inserted_id
