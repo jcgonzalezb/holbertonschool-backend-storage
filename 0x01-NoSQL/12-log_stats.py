b831467664acf15bafc77973ae6b657358d80631
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-03 14:03:19 -0500

    counters fixed

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -13,4 +13,16 @@ my_collection = my_database["nginx"]
 total_count = my_collection.count_documents({})
 print(total_count, "Logs")
 print("Methods:")
-
+total_count_get = my_collection.count_documents({"method": "GET"})
+print("\tmethod GET: ", total_count_get)
+total_count_post = my_collection.count_documents({"method": "POST"})
+print("\tmethod POST: ", total_count_post)
+total_count_put = my_collection.count_documents({"method": "PUT"})
+print("\tmethod PUT: ", total_count_put)
+total_count_patch = my_collection.count_documents({"method": "PATCH"})
+print("\tmethod PATCH: ", total_count_patch)
+total_count_delete = my_collection.count_documents({"method": "DELETE"})
+print("\tmethod DELETE: ", total_count_delete)
+total_count_status = my_collection.count_documents(
+    {"method": "GET", "path": "/status"})
+print(total_count_status, "status check")
