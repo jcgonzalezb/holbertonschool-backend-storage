2191a6c4f1840dab3e81f2db79334e7422f3bd19
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-04 19:45:55 -0500

    exercise updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 Redis caching module
 """
 import redis
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -18,7 +19,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store method that generates a random key
         (e.g. using uuid), store the input data
@@ -29,3 +30,28 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(
+            self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Returns the key's value.
+        fn is an optional argument that will be used to
+        convert the data back to the desired format.
+        """
+        result = self._redis.get(key)
+        if fn:
+            return fn(result)
+        return result
+
+    def get_str(self, key):
+        """
+        Returns key value as string.
+        """
+        return get(key, str)
+
+    def get_int(self, key):
+        """
+        Returns key value as integer.
+        """
+        return get(key, int)
