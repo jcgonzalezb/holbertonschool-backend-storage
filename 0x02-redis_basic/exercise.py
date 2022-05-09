a7b7387c10c80160e4350ff22558664ccfcbcabb
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-08 21:31:26 -0500

    mset implemented

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -89,7 +89,7 @@ class Cache:
                 Returns the random key.
         """
         random_key = str(uuid.uuid4())
-        self._redis.set(random_key, data)
+        self._redis.mset({random_key: data})
         return random_key
 
     def get(
